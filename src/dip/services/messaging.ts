8e038ad8eb2b22006066b16ebf1cc5e8a3872059
Naelz <nael.santos.nv11@gmail.com>
2025-06-17 09:22:41 -0300

    feature: create interfaces and dip.

--- src/dip/services/messaging.ts
+++ src/dip/services/messaging.ts
@@ -1,5 +1,6 @@
-export class Messaging {
+import { MessagingProtocol } from "../classes/interfaces/messaging-protocol";
 
+export class Messaging implements MessagingProtocol{
   sendMessage(msg: string): void {
     console.log('Mensagem enviada:', msg);
   }
